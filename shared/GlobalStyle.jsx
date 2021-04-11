6a9a252e09f34212a23e0f02bbdab9b4c99acdbc
Joan Andrés Lara Mora <joandres52752lm@gmail.com>
2021-04-11 13:03:18 -0500

    [Update] Table component and GlobalStyle

---
+++ shared/GlobalStyle.jsx
@@ -0,0 +1,10 @@
+import styled, { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+ html{
+  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+  font-size: 13px;
+ }
+`;
+
+export default GlobalStyle;
--- shared/components/Table.jsx
+++ shared/components/Table.jsx
@@ -1,8 +1,40 @@
 import styled from 'styled-components'
 
 const Table = styled.table`
- height: 100px;
- background-color: red;
+ width: 100%;
+ border-collapse: collapse;
+`;
+
+export const Row = Table.Row = styled.tr`
+`;
+
+export const Cell = Table.Cell = styled.td`
+ &.border{
+  border: 1px solid rgba(0, 0, 0, 0.1);
+ }
+
+ &.interactable{
+  cursor: pointer;
+
+  &:hover{
+   background-color: rgba(0, 0, 0, 0.1);
+  }
+
+  &.active{
+   background-color: red;
+  }
+ }
+`;
+
+export const Body = Table.Body = styled.tbody`
+`;
+
+export const Header = Table.Header = styled.thead`
+ font-weight: bold;
+ text-align: center;
+`;
+
+export const Head = Table.Head = styled.th`
 `;
 
 export default Table;
